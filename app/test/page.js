8d412b255e28ccea07835655b78b792985cc4252
KevTheFoo <kevthefoo@gmail.com>
2025-09-03 02:13:06 +1000

    Add vocabulary counting and querying functionality to the page component

---
+++ app/test/page.js
@@ -0,0 +1,36 @@
+"use client";
+
+import queryVocab from "@/lib/queryVocab";
+import countVocab from "@/lib/countVocab";
+
+import React from "react";
+
+const page = () => {
+    const queryVocabHandler = async () => {
+        const data = await queryVocab();
+        console.log(data);
+    };
+
+    const countVocabHandler = async () => {
+        const count = await countVocab();
+        console.log(count);
+    };
+    return (
+        <div className="flex h-full justify-center items-center border-4 border-red-400 gap-1">
+            <div
+                className="cursor-pointer p-2 border-2 border-black"
+                onClick={queryVocabHandler}
+            >
+                query
+            </div>
+            <div
+                className="cursor-pointer p-2 border-2 border-black"
+                onClick={countVocabHandler}
+            >
+                Count
+            </div>
+        </div>
+    );
+};
+
+export default page;
