f570e194dd6b0d34b90a6123c481aa17d48cc924
KevTheFoo <kevthefoo@gmail.com>
2025-09-11 00:05:19 +1000

    Refactor vocabulary navigation in Page component; add previous and next buttons for vocab review

--- app/review/page.js
+++ app/review/page.js
@@ -2,54 +2,52 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import randomQuery from "@/lib/randomQuery";
-import { Star, Volume2 } from "lucide-react";
-import addVocab from "@/lib/addVocab";
+import { ArrowBigLeft, ArrowBigRight, Volume2 } from "lucide-react";
 import playPronunce from "@/lib/playPronunce";
+
 const Page = () => {
   const [vocabData, setVocabData] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   const handleStart = async () => {
     const data = await randomQuery();
     setVocabData(data);
+    setCurrentIndex(0);
   };
 
-  const addVocabHandler = async () => {
-    const user_id = user.id;
-    const term = vocabData.vocabulary;
-    const descriptions = vocabData.descriptions;
-
-    const response = await addVocab(user_id, term, descriptions);
-    console.log(response);
+  const handlePreviousVocab = async () => {
+    setCurrentIndex((prev) => (prev > 0 ? prev - 1 : prev));
+  };
+  const handleNextVocab = async () => {
+    setCurrentIndex((prev) =>
+      vocabData && prev < vocabData.length - 1 ? prev + 1 : prev,
+    );
   };
 
   const playPronunceHandler = async () => {
-    const term = vocabData.vocabulary;
+    const term = vocabData[currentIndex].term;
     playPronunce(term);
   };
   return (
     <section className="flex h-full flex-col items-center justify-center border-4 border-red-400">
       <div>
-        {vocabData && (
+        {vocabData && vocabData[currentIndex] && (
           <div className="mb-8 w-full max-w-md rounded bg-white p-4 shadow">
             <div className="mb-2 flex items-center justify-between">
               <div className="flex items-center justify-center gap-2">
-                <h2 className="text-lg font-semibold">{vocabData[0].term}</h2>
+                <h2 className="text-lg font-semibold">
+                  {vocabData[currentIndex].term}
+                </h2>
 
                 <Volume2
                   className="cursor-pointer"
                   onClick={playPronunceHandler}
                 />
               </div>
-
-              <Star
-                className="cursor-pointer"
-                fill="yellow"
-                onClick={addVocabHandler}
-              />
             </div>
 
-            <p className="mb-2">{vocabData.meaning}</p>
             <ul className="flex flex-col gap-8 p-4">
-              {vocabData[0].descriptions.map((des, index) => (
+              {vocabData[currentIndex].descriptions.map((des, index) => (
                 <li key={index} className="list-disc">
                   <p className="font-semibold">{des.meaning}</p>
                   <i>{des.example}</i>
@@ -58,13 +56,32 @@ const Page = () => {
             </ul>
           </div>
         )}
-        <Button
-          onClick={handleStart}
-          disabled={false}
-          className="w-full cursor-pointer"
-        >
-          Start
-        </Button>
+        {vocabData ? (
+          <div className="flex items-center justify-center gap-4">
+            <Button
+              onClick={handlePreviousVocab}
+              disabled={currentIndex === 0}
+              className="cursor-pointer"
+            >
+              <ArrowBigLeft />
+            </Button>
+            <Button
+              onClick={handleNextVocab}
+              disabled={currentIndex === vocabData.length - 1}
+              className="cursor-pointer"
+            >
+              <ArrowBigRight />
+            </Button>
+          </div>
+        ) : (
+          <Button
+            onClick={handleStart}
+            disabled={false}
+            className="w-full cursor-pointer"
+          >
+            Start
+          </Button>
+        )}
       </div>
     </section>
   );
