a016c552aafbd110cc431532f9267248327b1318
KevTheFoo <kevthefoo@gmail.com>
2025-09-16 14:30:56 +1000

    set sign out redirect url

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider afterSignOutUrl="/">
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} flex h-screen flex-col antialiased`}
