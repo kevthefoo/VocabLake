d58c4bb4bf294d6ca2f968a8233517ee1a4693aa
KevTheFoo <kevthefoo@gmail.com>
2025-09-17 11:46:10 +1000

    feat: add Radix UI components and integrate Recharts for charting
    - Added new dependencies: @radix-ui/react-scroll-area, @radix-ui/react-separator, and recharts.
    - Implemented ScrollArea and ScrollBar components for enhanced scrolling functionality.
    - Created Separator component for visual separation in UI.
    - Developed Card component with subcomponents (Header, Title, Description, Action, Content, Footer) for structured content display.
    - Introduced ChartContainer, ChartTooltip, ChartTooltipContent, ChartLegend, and ChartLegendContent for chart rendering and interactivity.
    - Updated to-do list with new tasks and reviewing timeframes.

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,10 +1,14 @@
 "use client";
 
+import React from "react";
 import { useState, useEffect } from "react";
 import countVocab from "@/lib/countVocab";
-
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator"
 const Page = () => {
+  const tags = ["g", "dd", "jok"]
   const [vocabCount, setVocabCount] = useState(null);
+
   useEffect(() => {
     const fetchCount = async () => {
       const count = await countVocab();
@@ -23,7 +27,18 @@ const Page = () => {
 
   return (
     <section className="flex h-full items-center justify-center">
-      You have {vocabCount} Vocabs in your lake
+      <p> You have {vocabCount} Vocabs in your lake</p>
+      <ScrollArea className="h-72 w-48 rounded-md border">
+        <div className="p-4">
+          <h4 className="mb-4 text-sm leading-none font-medium">Tags</h4>
+          {tags.map((tag) => (
+            <React.Fragment key={tag}>
+              <div className="text-sm">{tag}</div>
+              <Separator className="my-2" />
+            </React.Fragment>
+          ))}
+        </div>
+      </ScrollArea>
     </section>
   );
 };
