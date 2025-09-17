50abeda4e59bc090e6485686263dae8d227531ec
KevTheFoo <kevthefoo@gmail.com>
2025-09-17 12:19:35 +1000

    feat: enhance pricing page layout with improved structure and heading

--- app/pricing/page.js
+++ app/pricing/page.js
@@ -3,7 +3,8 @@ import { neobrutalism } from "@clerk/themes";
 const page = () => {
   return (
     <section className="flex h-full items-center justify-center">
-      <div className="flex h-full w-1/2 items-center justify-center border-2 border-red-400">
+      <div className="flex h-full w-1/2 flex-col items-center justify-center border-2 border-red-400">
+        <h1 className="mb-4">Choose Your Plan</h1>
         <PricingTable
           className="border-2 border-blue-400"
           appearance={{
