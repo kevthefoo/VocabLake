bf56d038feca41ca135b6844b9fe9a3efb270ecc
KevTheFoo <kevthefoo@gmail.com>
2025-09-17 11:57:01 +1000

    feat: implement area chart with Recharts and enhance vocab display

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -4,9 +4,42 @@ import React from "react";
 import { useState, useEffect } from "react";
 import countVocab from "@/lib/countVocab";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/ui/separator";
+
+import { TrendingUp } from "lucide-react";
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+export const description = "A simple area chart";
 const Page = () => {
-  const tags = ["g", "dd", "jok"]
+  const tags = ["g", "dd", "jok"];
+  const chartData = [
+    { month: "January", desktop: 186 },
+    { month: "February", desktop: 305 },
+    { month: "March", desktop: 237 },
+    { month: "April", desktop: 73 },
+    { month: "May", desktop: 209 },
+    { month: "June", desktop: 214 },
+  ];
+  const chartConfig = {
+    desktop: {
+      label: "Desktop",
+      color: "var(--chart-1)",
+    },
+  };
   const [vocabCount, setVocabCount] = useState(null);
 
   useEffect(() => {
@@ -26,19 +59,78 @@ const Page = () => {
   }
 
   return (
-    <section className="flex h-full items-center justify-center">
-      <p> You have {vocabCount} Vocabs in your lake</p>
-      <ScrollArea className="h-72 w-48 rounded-md border">
-        <div className="p-4">
-          <h4 className="mb-4 text-sm leading-none font-medium">Tags</h4>
-          {tags.map((tag) => (
-            <React.Fragment key={tag}>
-              <div className="text-sm">{tag}</div>
-              <Separator className="my-2" />
-            </React.Fragment>
-          ))}
-        </div>
-      </ScrollArea>
+    <section className="flex h-full items-center justify-around gap-4 border-2 border-red-500">
+      <div className="w-1/2">
+        <Card>
+          <CardHeader>
+            <CardTitle>Area Chart</CardTitle>
+            <CardDescription>
+              Showing total visitors for the last 6 months
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ChartContainer config={chartConfig}>
+              <AreaChart
+                accessibilityLayer
+                data={chartData}
+                margin={{
+                  left: 12,
+                  right: 12,
+                }}
+              >
+                <CartesianGrid vertical={false} />
+                <XAxis
+                  dataKey="month"
+                  tickLine={false}
+                  axisLine={false}
+                  tickMargin={8}
+                  tickFormatter={(value) => value.slice(0, 3)}
+                />
+                <ChartTooltip
+                  cursor={false}
+                  content={<ChartTooltipContent indicator="line" />}
+                />
+                <Area
+                  dataKey="desktop"
+                  type="natural"
+                  fill="var(--color-desktop)"
+                  fillOpacity={0.4}
+                  stroke="var(--color-desktop)"
+                />
+              </AreaChart>
+            </ChartContainer>
+          </CardContent>
+          <CardFooter>
+            <div className="flex w-full items-start gap-2 text-sm">
+              <div className="grid gap-2">
+                <div className="flex items-center gap-2 leading-none font-medium">
+                  Trending up by 5.2% this month
+                  <TrendingUp className="h-4 w-4" />
+                </div>
+                <div className="text-muted-foreground flex items-center gap-2 leading-none">
+                  January - June 2024
+                </div>
+              </div>
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
+      <div className="flex h-full flex-col items-center justify-around border-2 border-black">
+        <p> You have {vocabCount} Vocabs in your lake</p>
+        <ScrollArea className="h-72 w-48 rounded-md border">
+          <div className="p-4">
+            <h4 className="mb-4 text-sm leading-none font-medium">
+              Recently Added Vocabs
+            </h4>
+            {tags.map((tag) => (
+              <React.Fragment key={tag}>
+                <div className="text-sm">{tag}</div>
+                <Separator className="my-2" />
+              </React.Fragment>
+            ))}
+          </div>
+        </ScrollArea>
+      </div>
     </section>
   );
 };
